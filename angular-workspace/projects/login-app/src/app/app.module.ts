19cc558e79bb764f68219b7ff285d628ea8a68ed
matteobavecchi <matteo.bavecchi@stud.unifi.it>
2021-05-20 09:50:01 +0200

    Builded the first webComponent

--- angular-workspace/projects/login-app/src/app/app.module.ts
+++ angular-workspace/projects/login-app/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,16 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const loginApp = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('app-login', loginApp);
+  }
+}
