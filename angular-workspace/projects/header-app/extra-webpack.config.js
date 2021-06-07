e034c9df4441187399f58fba8647c6bb596adcd2
matteobavecchi <matteo.bavecchi@stud.unifi.it>
2021-06-07 12:16:46 +0200

    Added extra-webpack.config

---
+++ angular-workspace/projects/header-app/extra-webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    output: {
+       // jsonpFunction: 'headerApp',
+        library: 'microfrontendHeader'
+    }
+};
---
+++ angular-workspace/projects/login-app/extra-webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    output: {
+       // jsonpFunction: 'loginApp',
+        library: 'microfrontendLogin'
+    }
+};
